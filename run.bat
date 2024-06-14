7649e9f894ecc67f61d7f447bb29998876c3df5a
basyair7 <basyair.fathul@gmail.com>
2024-06-14 21:53:00 +0700

    first commit program

--- run.bat
+++ run.bat
@@ -1,2 +1,7 @@
+@echo off
 cls;
 deno run --unstable-sloppy-imports --config tsconfig.json --import-map=import_map.json --allow-net --allow-read --allow-env src/server.ts
+
+echo Press any key to continue...
+pause > nul
+echo Continuing script...
