68f0d3cf4bf5cff71bfdbde0a92c83f6f6241ee3
basyair7 <basyair.fathul@gmail.com>
2024-06-14 21:33:46 +0700

    first commit program

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SimSimi Ahul 2 hemHem
 ### Project Gabut basyair
 ### Bedanya ini pake deno, satu lagi pake nodejs :v
+
+### cara install deno :
+- 1. buka powershell
+- 2. 
--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 cls;
-deno run --unstable-sloppy-imports --config tsconfig.json --import-map=import_map.json --allow-net --allow-read --allow-env src/server.ts;
+deno run --unstable-sloppy-imports --config tsconfig.json --import-map=import_map.json --allow-net --allow-read --allow-env src/server.ts
