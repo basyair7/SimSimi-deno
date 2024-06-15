b5b54a26824f2326662b3357f28e9f5d1aa0fe52
basyair7 <basyair.fathul@gmail.com>
2024-06-15 08:56:48 +0700

    add run-cache

--- test/server.ts
+++ test/server.ts
@@ -1,22 +1,41 @@
-import { TelegramBot, UpdateType } from "TelegramBot";
-import { config } from "dotenv";
+// import { TelegramBot, UpdateType } from "TelegramBot";
+// import { config } from "dotenv";
+
+// config({ export: true });
+
+// const TOKEN = Deno.env.get("TELEBOT_TOKEN");
+// if (!TOKEN) throw new Error("Bot token is not provided");
+// const bot = new TelegramBot(TOKEN);
+
+// bot.on(UpdateType.Message, async ({ message }) => {
+//   const text = message.text || "I can't hear you";
+//   await bot.sendMessage({
+//     chat_id: message.chat.id,
+//     text: text,
+//   });
+// });
+
+// bot.run({
+//   polling: true,
+// });
+
+// console.log("bot is running...");
+
+
+import { Bot } from "https://deno.land/x/grammy@v1.24.1/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
 config({ export: true });
-
 const TOKEN = Deno.env.get("TELEBOT_TOKEN");
 if (!TOKEN) throw new Error("Bot token is not provided");
-const bot = new TelegramBot(TOKEN);
 
-bot.on(UpdateType.Message, async ({ message }) => {
-  const text = message.text || "I can't hear you";
-  await bot.sendMessage({
-    chat_id: message.chat.id,
-    text: text,
-  });
+const bot: Bot = new Bot(TOKEN);
+
+bot.on("message:text", (ctx) => {
+  const chatId = ctx.message.chat.id;
+  const text = ctx.message.text;
+  ctx.reply(`${text} (${chatId})`);
 });
 
-bot.run({
-  polling: true,
-});
-
-console.log("bot is running...");
+bot.start();
+console.log("Bot is running...");
