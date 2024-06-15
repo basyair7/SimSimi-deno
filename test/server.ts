f73bc49128449646ef1daa74da6cc21d5bb76af0
basyair7 <basyair.fathul@gmail.com>
2024-06-15 12:20:18 +0700

    modified file run.bat

--- test/server.ts
+++ test/server.ts
@@ -1,27 +1,3 @@
-// import { TelegramBot, UpdateType } from "TelegramBot";
-// import { config } from "dotenv";
-
-// config({ export: true });
-
-// const TOKEN = Deno.env.get("TELEBOT_TOKEN");
-// if (!TOKEN) throw new Error("Bot token is not provided");
-// const bot = new TelegramBot(TOKEN);
-
-// bot.on(UpdateType.Message, async ({ message }) => {
-//   const text = message.text || "I can't hear you";
-//   await bot.sendMessage({
-//     chat_id: message.chat.id,
-//     text: text,
-//   });
-// });
-
-// bot.run({
-//   polling: true,
-// });
-
-// console.log("bot is running...");
-
-
 import { Bot } from "https://deno.land/x/grammy@v1.24.1/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
