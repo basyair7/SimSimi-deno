f49b1235c3a6b0d353b74bbfbc1f23b6879002a6
basyair7 <basyair.fathul@gmail.com>
2024-06-17 14:15:02 +0700

    modified file run.bat

--- run.bat
+++ run.bat
@@ -2,30 +2,37 @@
 cls
 setlocal
 
-:: Nama file yang akan dicek
-set "filename=.env"
+:: variable file environment
+set filename=.env
 
-:: Cek apakah file ada
-if exist "%filename%" (
-    @REM deno task test
-    @REM deno task dev
-    deno task start
+:: set configuration environment
+:: true or false
+set create_env=true
 
+:: deno task state
+:: test, dev, start
+set deno_task_state=start
+
+:: check if file env exist or not
+if %create_env% == true (
+    if exist "%filename%" (
+        deno task %deno_task_state%
+    ) else (
+        echo File %filename% tidak ditemukan, membuat file baru...
+        (
+            echo TELEBOT_TOKEN=
+            echo TELEBOT_USERNAME=
+            echo REGION=
+            echo SIMSIMI_APIURL=
+            echo SIMSIMI_APIKEYS=
+        ) > "%filename%"
+
+        echo File %filename% berhasil dibuat.
+        echo Silahkan isi environment bot, jika perlu bantuan hubungi author repositori ini "%filename%"
+    )
 ) else (
-    echo File %filename% tidak ditemukan, membuat file baru...
-    
-    (
-        echo TELEBOT_TOKEN=?
-        echo TELEBOT_USERNAME=?
-        echo REGION=?
-        echo SIMSIMI_APIURL=?
-        echo SIMSIMI_APIKEYS=?
-    ) > "%filename%"
-
-    echo File %filename% berhasil dibuat.
-    echo Silahkan isi environment bot, jika perlu bantuan hubungi author repositori ini "%filename%"
+    deno task %deno_task_state%
 )
-
 endlocal
 
 echo Press any key to continue...
