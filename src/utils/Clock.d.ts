f6653be38d4333741423d5975c38efffa2b7c0f7
basyair7 <basyair.fathul@gmail.com>
2024-06-21 11:46:49 +0700

    modified Clock.d.ts

--- src/utils/Clock.d.ts
+++ src/utils/Clock.d.ts
@@ -4,5 +4,7 @@ export declare class Clock {
     private local: DateTime | undefined;
     private rezonedString: DateTime | undefined;
 
+    constructor(serverTimeZone?: string);
+
     public static get(serverTimeZone?: string): string;
 }
