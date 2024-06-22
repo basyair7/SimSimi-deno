53b4756db6372da3f66858535657856da72eef10
basyair7 <basyair.fathul@gmail.com>
2024-06-23 00:14:34 +0700

    add setcommands

--- src/commands/StartCommand.ts
+++ src/commands/StartCommand.ts
@@ -7,7 +7,7 @@ class StartCommand implements CommandHandler {
     readonly description = 'Start the bot';
     
     public execute(ctx: Context): void {
-        ctx.reply("Welcome! How can I assist you today? Get /help commands?");
+        ctx.reply("Welcome! how can I assist you today? Get /menu commands? or setup your bot /setcommands");
     }
 }
 
