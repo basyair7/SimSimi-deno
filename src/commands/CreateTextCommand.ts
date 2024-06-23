c719aff4dd4bc5374c438046cde83628a5724938
basyair7 <basyair.fathul@gmail.com>
2024-06-23 10:21:38 +0700

    modified commands : CreateTextCommand, HelpCommand, SetCommand, StartCommand

--- src/commands/CreateTextCommand.ts
+++ src/commands/CreateTextCommand.ts
@@ -26,7 +26,7 @@ class CreateTextCommands implements CommandHandler {
         let _menuMsg: string = "Creating a text file : \n";
         try {
             for await (const entry of Deno.readDir(this.commandDir)) {
-                if (entry.isFile && entry.name.endsWith('.ts')&& !entry.name.endsWith('.disable.ts')) {
+                if (entry.isFile && entry.name.endsWith('.ts') && !entry.name.endsWith('.disable.ts')) {
                     const _filePath: string = path.join(this.commandDir, entry.name);
                     const _fileUrl: string = path.toFileUrl(_filePath).href;
                     const _commandModule = await import(_fileUrl);
